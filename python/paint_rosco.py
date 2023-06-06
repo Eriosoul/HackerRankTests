7d78b2641dd1c16a7aa123e6a81c887fc44a2120
Eriosoul <tboss1780@gmail.com>
2023-06-06 22:01:22 +0200

    piramide

---
+++ python/paint_rosco.py
@@ -0,0 +1,23 @@
+def draw_spiral(size):
+    # ╗  ═ ╚
+    for row in range(0, size):
+        spiral = ""
+        horizontal = row == 0
+        for col in range(0, size):
+            if row + col == size -1:
+                spiral += "╗" if col >= row else "╚"
+                horizontal = col < row
+            elif row - col == 1 and row < (size / 2):
+                spiral += "╔"
+                horizontal = True
+            elif row == col and row >= (size / 2):
+                spiral += "╝"
+                horizontal = False
+            else:
+                spiral += "═" if horizontal else "║"
+
+        print(spiral)
+
+draw_spiral(5)
+draw_spiral(20)
+draw_spiral(50)
