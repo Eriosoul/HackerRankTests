9b34819311909b8af5dffaeac6efaeb4b5046718
Eriosoul <tboss1780@gmail.com>
2023-07-03 01:41:33 +0200

    binary,hexa,octal

--- python/decimal_to_binary.py
+++ python/decimal_to_binary.py
@@ -1,10 +1,28 @@
-def binario(decimal):
-    binario = ""
-    while decimal // 2 != 0:
-        binario = str(decimal % 2) + binario
-        decimal = decimal // 2
-    return str(decimal) + binario
+def binario(decimal_binary):
+    binary = ""
+    while decimal_binary // 2 != 0:
+        binary = str(decimal_binary % 2) + binary
+        decimal_binary = decimal_binary // 2
+    return str(decimal_binary) + binary
 
-numero = int(input("Introduce un numero y se convertira a binario: "))
+def hexadecimal(decimal_hexa):
+    hexa = ""
+    while decimal_hexa // 16 != 0:
+        hexa = str(decimal_hexa % 16) + hexa
+        decimal_hexa = decimal_hexa // 16
+    return str(decimal_hexa) + hexa
 
-print(binario(numero))
+
+def octal(decimal_octal):
+    octa = ""
+    while decimal_octal // 8 != 0:
+        octa = str(decimal_octal % 8) + octa
+        decimal_octal = decimal_octal // 8
+    return str(decimal_octal) + octa
+
+num = int(input("Introduce un numero y se convertira a binario: "))
+
+print(binario(num))
+print(hexadecimal(num))
+print(octal(num))
+
