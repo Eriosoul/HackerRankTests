843ca5b5fccbd6ea0c3f3fcd48416490fd3bad63
Eriosoul <tboss1780@gmail.com>
2023-07-02 17:18:11 +0200

    Adding, a draw photo, and a till deadline

---
+++ python/models/time-till-deadline.py
@@ -0,0 +1,21 @@
+import datetime
+import time
+
+user_input = input("Enter your goal whit your deadline, separated by colon: \n")
+input_list = user_input.split(":")
+
+goal = input_list[0]
+deadline = input_list[1]
+
+deadline_date = datetime.datetime.strptime(deadline, "%d.%m.%Y")
+
+today_date = datetime.datetime.today()
+time_till = deadline_date - today_date
+
+print(f"Time remaining for your goal : {goal} is {time_till}")
+
+
+# while True:
+#     date = datetime.datetime.today()
+#     time.sleep(1)
+#     print(date)
