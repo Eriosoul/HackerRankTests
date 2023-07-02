d260afaa1aade678dca00255319f875521638354
Eriosoul <tboss1780@gmail.com>
2023-07-02 17:33:42 +0200

    till deadline

--- python/models/time-till-deadline.py
+++ python/models/time-till-deadline.py
@@ -11,8 +11,8 @@ deadline_date = datetime.datetime.strptime(deadline, "%d.%m.%Y")
 
 today_date = datetime.datetime.today()
 time_till = deadline_date - today_date
-
-print(f"Time remaining for your goal : {goal} is {time_till}")
+hours_till = int(time_till.total_seconds() / 60 / 60)
+print(f"Time remaining for your goal : {goal} is {hours_till} hours")
 
 
 # while True:
